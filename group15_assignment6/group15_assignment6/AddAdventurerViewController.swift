2fcd26f004f77845c9a6db8f05c73d29f0c05b24
Ohyoon Kwon <ohyoon425@gmail.com>
2019-10-15 17:10:45 -0500

    storyboard

---
+++ group15_assignment6/group15_assignment6/AddAdventurerViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AddAdventurerViewController.swift
+//  group15_assignment6
+//
+//  Created by Ohyoon Kwon on 15/10/2019.
+//
+
+import UIKit
+
+class AddAdventurerViewController: UIViewController {
+    
+    //MARK: outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var classLabel: UILabel!
+    @IBOutlet weak var appearanceLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var classTextField: UITextField!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ group15_assignment6/group15_assignment6/QuestViewController.swift
@@ -0,0 +1,33 @@
+//
+//  QuestViewController.swift
+//  group15_assignment6
+//
+//  Created by Ohyoon Kwon on 15/10/2019.
+//
+
+import UIKit
+
+class QuestViewController: UIViewController {
+    
+    var timer = Timer()
+    //timer = Timer.scheduledTimer(timeInterval: [time interval], target: self, selector: #selector([function to call]), userInfo: nil, repeats: true)
+    //timer.invalidate()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
