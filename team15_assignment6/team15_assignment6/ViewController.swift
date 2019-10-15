57a2d693e65420a076722d6b264235bd8a4563d5
Ohyoon Kwon <ohyoon425@gmail.com>
2019-10-15 16:04:54 -0500

    created file

---
+++ team15_assignment6/team15_assignment6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  team15_assignment6
+//
+//  Created by Ohyoon Kwon on 15/10/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
