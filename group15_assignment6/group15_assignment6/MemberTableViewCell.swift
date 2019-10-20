0a95e3f53bbfa310c459b2870716e976d1d5371b
Colton Ragland <cragland@utexas.edu>
2019-10-20 02:58:33 -0500

    Class for cell items and UI adjustments

---
+++ group15_assignment6/group15_assignment6/MemberTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MemberTableViewCell.swift
+//  group15_assignment6
+//
+//  Created by Colton R on 10/20/19.
+//
+
+import UIKit
+
+class MemberTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var portraitImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var professionLabel: UILabel!
+    @IBOutlet weak var attackMod: UILabel!
+    @IBOutlet weak var HPLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var HPValLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
