cda111e37a0a0345eff4769dfea4d0846f79ac9a
Colton Ragland <cragland@utexas.edu>
2019-10-26 18:28:07 -0500

    Set up Quest log mechanism

--- group15_assignment6/group15_assignment6/QuestViewController.swift
+++ group15_assignment6/group15_assignment6/QuestViewController.swift
@@ -18,11 +18,48 @@ class QuestViewController: UIViewController {
     @IBOutlet weak var professionLabel: UILabel!
     @IBOutlet weak var portrait: UIImageView!
     @IBOutlet weak var levelLabel: UILabel!
+    var turn = 0;
+    var enemyHP = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //timer = Timer.scheduledTimer(timeInterval: [time interval], target: self, selector: #selector([function to call]), userInfo: nil, repeats: true)
+        let _ = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(questTurn), userInfo: nil, repeats: true)
+    }
+    
+    @objc func questTurn() {
+        // Set enemy
+        if (enemyHP <= 0) {
+            enemyHP = Int(arc4random_uniform(50)) + 100;
+            if (turn != 0) {questDetailText.text += "\nEnemy is defeated!\nA new enemy appears!"}
+        }
+        // Adventurer makes move
+        let attack = (Int(arc4random_uniform(4)) != 0);
+        if (turn % 2 == 0) {
+            if (attack) {
+                // Access MEMBER_ATTACK and use it as a multplier.
+                var temp = (attackLabel.text! as NSString).doubleValue;
+                temp = round(temp * 15);
+                let damage = Int(temp) + Int(arc4random_uniform(10));
+                enemyHP -= Int(damage);
+                // Access MEMBER_NAME to print to log
+                questDetailText.text += "\n\(nameLabel.text ?? "") attacks for \(damage)";
+            }
+            else {questDetailText.text += "\n\(nameLabel.text ?? "") is waiting...";}
+        }
+        // Monster makes move
+        else {
+            if (attack) {
+                let damage = Int(arc4random_uniform(10)) + 5;
+                // subtact from MEMBER_HP
+                questDetailText.text += "\nMonster attacks for \(damage)"
+                
+            }
+            else {questDetailText.text += "\nThe monster is waiting...";}
+        }
+        turn += 1;
+        // Close window if  MEMBER_HP falls to zero:
+        // if ((attackLabel.text! as NSString).intValue <= 0) {dismiss(animated: true, completion: nil)}
     }
     
     // MARK: Action
