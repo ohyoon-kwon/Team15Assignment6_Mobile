61cfe96cf70e951b80e68ff0ed100eacfaf1de6b
Amber Xuxian Chen <ambercxx@utexas.edu>
2019-10-28 18:11:20 -0500

    displayed image

--- group15_assignment6/group15_assignment6/QuestViewController.swift
+++ group15_assignment6/group15_assignment6/QuestViewController.swift
@@ -10,17 +10,18 @@ import CoreData
 
 class QuestViewController: UIViewController {
     
-    var timer: Timer?
-    var Adventurers: [NSManagedObject] = []
-    var adventurerID : Int?
-    
     @IBOutlet weak var questDetailText: UITextView!
     @IBOutlet weak var attackLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var hpLabel: UILabel!
+    @IBOutlet weak var curLabel: UILabel!
+    @IBOutlet weak var totLabel: UILabel!
     @IBOutlet weak var professionLabel: UILabel!
     @IBOutlet weak var portrait: UIImageView!
     @IBOutlet weak var levelLabel: UILabel!
+    
+    var timer: Timer?
+    var Adventurers: [NSManagedObject] = []
+    var adventurerID : Int?
     var turn = 0;
     var enemyHP = 0;
     var monstersSlayed = 0
@@ -28,9 +29,49 @@ class QuestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let _ = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(questTurn), userInfo: nil, repeats: true)
+ /*       let _ = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(questTurn), userInfo: nil, repeats: true)*/
     }
     
+    // MARK: Action
+    @IBAction func endquest(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    func getAdventurer(ID : Int){
+        let adventurer = Adventurers[ID]
+        print(adventurer)
+        if let imageData = adventurer.value(forKey: "portrait") as? Data {
+            portrait?.image = UIImage(data: imageData)}
+        nameLabel.text = adventurer.value(forKeyPath: "name") as? String
+        levelLabel.text = adventurer.value(forKeyPath: "level") as? String
+        professionLabel.text = adventurer.value(forKeyPath: "profession") as? String
+        /*attackLabel.text = adventurer.value(forKeyPath: "attackMod")! as? String
+        totLabel.text = adventurer.value(forKeyPath: "totalHP")! as? String
+        curLabel.text = totLabel.text*/
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        //2
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Adventurer")
+        //3
+        do {
+            Adventurers = try managedContext.fetch(fetchRequest)
+            getAdventurer(ID: adventurerID!)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+   /*
     @objc func questTurn() {
         // Set enemy
         if (enemyHP <= 0) {
@@ -52,14 +93,14 @@ class QuestViewController: UIViewController {
             }
             else {questDetailText.text += "\n\(nameLabel.text ?? "") is waiting...";}
         }
-        // Monster makes move
+            // Monster makes move
         else {
             if (attack) {
                 let damage = Int(arc4random_uniform(10)) + 5;
                 //let damage = 1
                 // subtact from MEMBER_HP
-                let hp = (hpLabel.text! as NSString).intValue
-                hpLabel.text = String(hp - Int32(damage))
+                let hp = (curLabel.text! as NSString).intValue
+                curLabel.text = String(hp - Int32(damage))
                 questDetailText.text += "\nMonster attacks for \(damage)"
                 
             }
@@ -67,7 +108,7 @@ class QuestViewController: UIViewController {
         }
         turn += 1;
         // Close window if  MEMBER_HP falls to zero:
-        if ((hpLabel.text! as NSString).intValue <= 0) {
+        if ((curLabel.text! as NSString).intValue <= 0) {
             dismiss(animated: true, completion: nil)
             
         }
@@ -77,54 +118,7 @@ class QuestViewController: UIViewController {
             monstersSlayed = 0;
         }
     }
-    
-    // MARK: Action
-    @IBAction func endquest(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
-
-    }
-    
-    func getAdventurer(ID : Int){
-        let adventurer = Adventurers[ID]
-        print(adventurer)
-        //portrait.image =
-        nameLabel.text = adventurer.value(forKeyPath: "name") as? String
-        levelLabel.text = adventurer.value(forKeyPath: "level") as? String
-        professionLabel.text = adventurer.value(forKeyPath: "profession") as? String
-        attackLabel.text = adventurer.value(forKeyPath: "attackMod") as? String
-        let curHP = adventurer.value(forKeyPath: "currentHP") as? String
-        let totHP = adventurer.value(forKeyPath: "totalHP") as? String
-        print(curHP)
-        print(totHP)
-        hpLabel.text = "\(curHP)/\(totHP)"
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        //1
-        guard let appDelegate =
-            UIApplication.shared.delegate as? AppDelegate else {
-                return
-        }
-        
-        let managedContext =
-            appDelegate.persistentContainer.viewContext
-        
-        //2
-        let fetchRequest =
-            NSFetchRequest<NSManagedObject>(entityName: "Adventurer")
-        
-        //3
-        do {
-            Adventurers = try managedContext.fetch(fetchRequest)
-            getAdventurer(ID: adventurerID!)
-
-        } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
-        }
-    }
-    
+ */
     /*
     // MARK: - Navigation
 
